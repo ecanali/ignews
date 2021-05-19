ba9e7f95db9e62c06321f57d579dff3f9837b570
Erick Canali <erick.canali@gmail.com>
2021-05-19 08:57:59 -0300

    minor update

--- src/pages/api/subscribe copy.ts
+++ src/pages/api/subscribe copy.ts
@@ -45,17 +45,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         )
       )
 
-      await fauna.query(
-        q.Update(
-          q.Ref(q.Collection('users'), user.ref.id),
-          {
-            data: {
-              stripe_customer_id: stripeCustomer.id,
-            }
-          }
-        )
-      )
-
       customerId = stripeCustomer.id;
     }
 
